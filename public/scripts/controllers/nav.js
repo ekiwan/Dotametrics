a651665e4edb72d16f28319566b65b896fad32f5
ekiwan <elie.kiwan@gmail.com>
2013-09-16 16:34:19 -0700

    start building nav buttons to switch charts. problems with scoping

--- public/scripts/controllers/nav.js
+++ public/scripts/controllers/nav.js
@@ -1,3 +1,8 @@
 angular.module('app.controllers.nav', [])
-  .controller('NavController', ['$scope', function ($scope) { 
+  .controller('NavController', ['$scope', function ($scope) {
+    console.log()
+    // $scope.chartState.show = false;
+    // $scope.toggleChart = function() {
+    //   $scope.chartState.show = !$scope.chartState.show;
+    // } 
   }]);
