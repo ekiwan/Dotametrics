65c87a31ab05cb88b9730be3b84e2f9ec39a1226
ekiwan <elie.kiwan@gmail.com>
2013-09-17 15:49:12 -0700

    finish win rate controller

--- public/scripts/controllers/winrate.js
+++ public/scripts/controllers/winrate.js
@@ -2,21 +2,21 @@ angular.module('app.controllers.winrate', ['app.services.data', 'app.services.op
   .controller('WinrateController', ['$scope', 'Data', 'Options', function ($scope, Data, Options) { 
     console.log('winrate chart controller', $scope)
     $scope.chart = {
-      labels: ["Kills", "Deaths", "Assists"],
-      datasets: [
+      labels : ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept"],
+      datasets : [
         {
-          fillColor : "#e67e22",
+          fillColor : "rgba(151,187,205,0)",
           strokeColor : "#e67e22",
-          pointColor : "#e67e22",
+          pointColor : "rgba(151,187,205,0)",
           pointStrokeColor : "#e67e22",
-          data: Data.data.winrateData[0]
+          data : Data.data.winrateData[0]
         },
         {
-          fillColor : "#f1c40f",
+          fillColor : "rgba(151,187,205,0)",
           strokeColor : "#f1c40f",
-          pointColor : "#f1c40f",
+          pointColor : "rgba(151,187,205,0)",
           pointStrokeColor : "#f1c40f",
-          data: Data.data.winrateData[1]
+          data : Data.data.winrateData[1]
         }
       ],
     };
--- public/scripts/services/options.js
+++ public/scripts/services/options.js
@@ -1,50 +1,135 @@
 angular.module('app.services.options', [])
   .factory('Options', [function () { 
     return {
-      barOptions: {
-        //Boolean - If we show the scale above the chart data     
-        scaleOverlay : true,
-        //Boolean - If we want to override with a hard coded scale
-        scaleOverride : true,
-        //** Required if scaleOverride is true **
-        //Number - The number of steps in a hard coded scale
-        scaleSteps : 10,
-        //Number - The value jump in the hard coded scale
-        scaleStepWidth : 1,
-        //Number - The scale starting value
-        scaleStartValue : 0,
-      },
+      barOptions:
+        {
+          //Boolean - If we show the scale above the chart data     
+          scaleOverlay : true,
+          //Boolean - If we want to override with a hard coded scale
+          scaleOverride : true,
+          //** Required if scaleOverride is true **
+          //Number - The number of steps in a hard coded scale
+          scaleSteps : 10,
+          //Number - The value jump in the hard coded scale
+          scaleStepWidth : 1,
+          //Number - The scale starting value
+          scaleStartValue : 0,
+        },
 
-      kdaOptions: {
-        //Boolean - Whether we should show a stroke on each segment
-        segmentShowStroke : true,
+      kdaOptions:
+        {
+          //Boolean - Whether we should show a stroke on each segment
+          segmentShowStroke : true,
 
-        //String - The colour of each segment stroke
-        segmentStrokeColor : "#fff",
+          //String - The colour of each segment stroke
+          segmentStrokeColor : "#fff",
 
-        //Number - The width of each segment stroke
-        segmentStrokeWidth : 24,
+          //Number - The width of each segment stroke
+          segmentStrokeWidth : 24,
 
-        //The percentage of the chart that we cut out of the middle.
-        percentageInnerCutout : 50,
+          //The percentage of the chart that we cut out of the middle.
+          percentageInnerCutout : 50,
 
-        //Boolean - Whether we should animate the chart
-        animation : true,
+          //Boolean - Whether we should animate the chart
+          animation : true,
 
-        //Number - Amount of animation steps
-        animationSteps : 100,
+          //Number - Amount of animation steps
+          animationSteps : 100,
 
-        //String - Animation easing effect
-        animationEasing : "easeOutBounce",
+          //String - Animation easing effect
+          animationEasing : "easeOutBounce",
 
-        //Boolean - Whether we animate the rotation of the Doughnut
-        animateRotate : true,
+          //Boolean - Whether we animate the rotation of the Doughnut
+          animateRotate : true,
 
-        //Boolean - Whether we animate scaling the Doughnut from the centre
-        animateScale : false,
+          //Boolean - Whether we animate scaling the Doughnut from the centre
+          animateScale : false,
 
-        //Function - Will fire on animation completion.
-        onAnimationComplete : null
-      }
+          //Function - Will fire on animation completion.
+          onAnimationComplete : null
+        },
+
+      winrateOptions:
+        {
+          //Boolean - If we show the scale above the chart data     
+          scaleOverlay : false,
+          
+          //Boolean - If we want to override with a hard coded scale
+          scaleOverride : false,
+          
+          //** Required if scaleOverride is true **
+          //Number - The number of steps in a hard coded scale
+          scaleSteps : null,
+          //Number - The value jump in the hard coded scale
+          scaleStepWidth : null,
+          //Number - The scale starting value
+          scaleStartValue : null,
+
+          //String - Colour of the scale line 
+          scaleLineColor : "rgba(0,0,0,.3)",
+          
+          //Number - Pixel width of the scale line  
+          scaleLineWidth : 1,
+
+          //Boolean - Whether to show labels on the scale 
+          scaleShowLabels : true,
+          
+          //Interpolated JS string - can access value
+          scaleLabel : "<%=value%>",
+          
+          //String - Scale label font declaration for the scale label
+          scaleFontFamily : "'Arial'",
+          
+          //Number - Scale label font size in pixels  
+          scaleFontSize : 12,
+          
+          //String - Scale label font weight style  
+          scaleFontStyle : "normal",
+          
+          //String - Scale label font colour  
+          scaleFontColor : "#666",  
+          
+          ///Boolean - Whether grid lines are shown across the chart
+          scaleShowGridLines : true,
+          
+          //String - Colour of the grid lines
+          scaleGridLineColor : "rgba(0,0,0,.1)",
+          
+          //Number - Width of the grid lines
+          scaleGridLineWidth : 1, 
+          
+          //Boolean - Whether the line is curved between points
+          bezierCurve : true,
+          
+          //Boolean - Whether to show a dot for each point
+          pointDot : true,
+          
+          //Number - Radius of each point dot in pixels
+          pointDotRadius : 3,
+          
+          //Number - Pixel width of point dot stroke
+          pointDotStrokeWidth : 1,
+          
+          //Boolean - Whether to show a stroke for datasets
+          datasetStroke : true,
+          
+          //Number - Pixel width of dataset stroke
+          datasetStrokeWidth : 2,
+          
+          //Boolean - Whether to fill the dataset with a colour
+          datasetFill : true,
+          
+          //Boolean - Whether to animate the chart
+          animation : true,
+
+          //Number - Number of animation steps
+          animationSteps : 60,
+          
+          //String - Animation easing effect
+          animationEasing : "easeInOutElastic",
+
+          //Function - Fires when the animation is complete
+          onAnimationComplete : null
+        }
     };
   }]);
