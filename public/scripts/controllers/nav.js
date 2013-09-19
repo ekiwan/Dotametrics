ab63a318001f25d7af7bb4d5ab3316ce7c9a0c1d
ekiwan <elie.kiwan@gmail.com>
2013-09-19 12:54:43 -0700

    add nav functionality to sidebar

--- public/scripts/controllers/nav.js
+++ public/scripts/controllers/nav.js
@@ -1,4 +1,7 @@
 angular.module('app.controllers.nav', [])
-  .controller('NavController', ['$scope', function ($scope) {
-    console.log('nav controller')
+  .controller('NavController', ['$scope', '$location', function ($scope, $location) {
+    console.log('nav controller', $location.path());
+    $scope.changePath = function(path) {
+      $location.path(path);
+    }
   }]);
