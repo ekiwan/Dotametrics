fea594081ab26a680f2afe2ad55e26a75464c3ed
ekiwan <elie.kiwan@gmail.com>
2013-09-24 15:43:46 -0700

    add countLastHits method to Replay

--- replay.js
+++ replay.js
@@ -1,5 +1,13 @@
 var cs = require('./data/cs.js');
-
+var denies = require('./data/denies.js');
 module.exports = {
-
+  countLastHits: function(heroName) {
+    var count = 0;
+    for (var i = 0; i < cs.length; i++) {
+      if (cs[i].hero === heroName) {
+        count++;
+      }
+    }
+    return count;
+  }
 };
