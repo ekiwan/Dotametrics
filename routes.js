4f205a25ef3f403217830195b0d9b7b3f911fc00
ekiwan <elie.kiwan@gmail.com>
2013-09-24 14:19:29 -0700

    fix async issues with dazzle api

--- routes.js
+++ routes.js
@@ -1,34 +1,67 @@
-module.exports = function(app) {
+var dazzle = require('dazzle');
+var http = require('http');
 
-  var dazzle = require('dazzle');
+var resolveVanityUrl = function(name, cb) {
+  http.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + global.config.APIKEY + '&vanityurl=' + name, function(res) {
+    
+    // Gather chunks of data
+    var response = '';
+    res.on('data', function(chunk) {
+      response += chunk;
+    });
+
+    // When the response is complete
+    res.on('end', function() {
+      try {
+        // Attempt to parse the JSON
+        var obj = JSON.parse(response);
+      }
+      catch(err) {
+        // JSON parse failed
+        return cb(err, null);
+      }
+
+      // Success
+      console.log('Fetched Steam ID: ', obj, obj.response.steamid)      
+
+      if (obj.response.success !== 1) {
+        return cb(new Error('Failed to fetch Steam ID'), null);
+      }
+
+      // Call callback if it is successful
+      cb(null, obj.response.steamid);
+    });
+
+  }).on('error', function(err) {
+    // Failed to fetch Steam ID
+    console.log('Error fetching Steam ID: ' + err);
+    cb(err, null);
+  });
+};
+
+module.exports = function(app) {
   var dota2Api = new dazzle(global.config.APIKEY);
 
-  var http = require('http');
-  var apiKey = global.config.APIKEY;
+  app.get('/api/getMatches/:steamId', function(req, res, next) {
+    console.log('Fetching Account ID for '+req.params.steamId);
 
-  var resolveVanityUrl = function(name) {
-    http.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + apiKey + '&vanityurl=' + name, function(res) {
-      res.on('data', function(chunk) {
-        var response = JSON.parse(chunk.toString());
-        console.log(response.response.steamid)
-        return response.response.steamid;
-      })
-    }).on('error', function(err) {
-      console.log("error: " + err.message);
-    })
-  };
-  var account_id = resolveVanityUrl('gbks');
-  console.log(account_id);
+    // Resolve the account ID from the steam vanity name
+    resolveVanityUrl(req.params.steamId, function(err, accountId) {
+      // Send error if it failed
+      if (err) return res.json({ status: 0, message: err.message });
 
-  app.get('/api/getMatches', function(req, res, next) {
-    req.params.account_id = '76561197969394835';
-    console.log(res.body)
-    dota2Api.getMatchHistory(req.params, function (err, response) {
-      if(err) {
-        return next(err);
-      }
-      res.json(response);
+      console.log('Fetching history for Account ID '+accountId);
+
+      // Get history
+      dota2Api.getMatchHistory({ account_id: accountId }, function (err, response) {
+        if (err) return res.json({ status: 0, message: err.message });
+
+        // Send response
+        console.log(response)
+        res.json(response);
+      });
     });
+
   });
 };
 
