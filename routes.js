2fdf8bffff30a76511125ad56c6218d0d5b88230
ekiwan <elie.kiwan@gmail.com>
2013-09-23 20:48:28 -0700

    converting steam vanity name to 64-bit id

--- routes.js
+++ routes.js
@@ -3,9 +3,26 @@ module.exports = function(app) {
   var dazzle = require('dazzle');
   var dota2Api = new dazzle(global.config.APIKEY);
 
+  var http = require('http');
+  var apiKey = global.config.APIKEY;
+
+  var resolveVanityUrl = function(name) {
+    http.get('http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/?key=' + apiKey + '&vanityurl=' + name, function(res) {
+      res.on('data', function(chunk) {
+        var response = JSON.parse(chunk.toString());
+        console.log(response.response.steamid)
+        return response.response.steamid;
+      })
+    }).on('error', function(err) {
+      console.log("error: " + err.message);
+    })
+  };
+  var account_id = resolveVanityUrl('gbks');
+  console.log(account_id);
+
   app.get('/api/getMatches', function(req, res, next) {
-    req.params.matches_requested = 2;
-    console.log(req.params)
+    req.params.account_id = '76561197969394835';
+    console.log(res.body)
     dota2Api.getMatchHistory(req.params, function (err, response) {
       if(err) {
         return next(err);
